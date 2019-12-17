2f7106ca9abf6fb630405e74c8a74cf32ffc0a1f
Wildan Ziaulhaq <wildanzhq@gmail.com>
2019-12-17 18:53:03 +0700

    Init

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "infobola-v1";
+var urlsToCache = [
+    "/",
+    "/manifest.json",
+    "/index.html",
+    "/css/materialize.min.css",
+    "/css/style.css",
+    "/img/icon.png",
+    "/img/icon192.png",
+    "/js/main.js"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request, { cacheName: CACHE_NAME })
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                var fetchRequest = event.request.clone();
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        if (!response || response.status !== 200) {
+                            return response;
+                        }
+                        var responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('Aktivasi service worker baru');
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME && cacheName.startsWith("tutorial")) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
