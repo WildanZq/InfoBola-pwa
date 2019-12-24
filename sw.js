f7aa14dd3c65b0b1e80e00a43f28c410eb984711
Wildan Ziaulhaq <wildanzhq@gmail.com>
2019-12-25 06:43:45 +0700

    Minor fix

--- sw.js
+++ sw.js
@@ -6,22 +6,22 @@ else
     console.log(`Workbox gagal dimuat`);
 
 workbox.precaching.precacheAndRoute([
-    { url: '/', revision: '1' },
-    { url: '/manifest.json', revision: '1' },
-    { url: '/index.html', revision: '1' },
-    { url: '/team.html', revision: '1' },
-    { url: '/pages/home.html', revision: '1' },
-    { url: '/pages/saved.html', revision: '1' },
-    { url: '/css/materialize.min.css', revision: '1' },
-    { url: '/css/style.css', revision: '1' },
-    { url: '/img/icon.png', revision: '1' },
-    { url: '/img/icon192.png', revision: '1' },
-    { url: '/js/script.js', revision: '1' },
-    { url: '/js/main.js', revision: '1' },
-    { url: '/js/api.js', revision: '1' },
-    { url: '/js/db.js', revision: '1' },
-    { url: '/js/idb.js', revision: '1' },
-    { url: '/js/team.js', revision: '1' },
+    { url: './', revision: '1' },
+    { url: 'manifest.json', revision: '1' },
+    { url: 'index.html', revision: '1' },
+    { url: 'team.html', revision: '1' },
+    { url: 'pages/home.html', revision: '1' },
+    { url: 'pages/saved.html', revision: '1' },
+    { url: 'css/materialize.min.css', revision: '1' },
+    { url: 'css/style.css', revision: '1' },
+    { url: 'img/icon.png', revision: '1' },
+    { url: 'img/icon192.png', revision: '1' },
+    { url: 'js/script.js', revision: '1' },
+    { url: 'js/main.js', revision: '1' },
+    { url: 'js/api.js', revision: '1' },
+    { url: 'js/db.js', revision: '1' },
+    { url: 'js/idb.js', revision: '1' },
+    { url: 'js/team.js', revision: '1' },
 ]);
 
 workbox.routing.registerRoute(
